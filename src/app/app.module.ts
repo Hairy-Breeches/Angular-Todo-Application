e1c667f8b85cd49ed4fc14fc999fd246fa197cd4
Hairy-Breeches <boooom86608@gmail.com>
2023-08-04 17:00:31 +0500

    Added To-do UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      'primary-1': '#00E995',
+      'primary-2': '#112D4E',
+      'secondary': '#AAAAAA',
+      'soft': '#DBE2EF',
+      'background': '#FFFFFF'
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+}
